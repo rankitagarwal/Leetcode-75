44f1cfa97f1573375b245edf75160bdd51ccbd35
Rankit <rankit.agarwal97@gmail.com>
2025-04-10 22:38:37 +0530

    leetcode 14

--- Leetcode14.playground/Contents.swift
+++ Leetcode14.playground/Contents.swift
@@ -1,3 +1,18 @@
-import UIKit
+class Solution {
+    func findMaxAverage(_ nums: [Int], _ k: Int) -> Double {
+        var maxAvg: Double = 0
+        var sum: Double = 0
+        for i in 0..<k {
+            sum += Double(nums[i])
+        }
 
-var greeting = "Hello, playground"
+        maxAvg = Double(sum) / Double(k)
+        
+        for j in k..<nums.count {
+            sum = sum + Double(nums[j] - nums[j - k])
+            maxAvg = max(maxAvg, sum / Double(k))
+        }
+
+        return maxAvg
+    }
+}
