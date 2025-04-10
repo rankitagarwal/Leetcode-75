d9378bf8646c84580bc170520ff87140c621ccc6
Rankit <rankit.agarwal97@gmail.com>
2025-04-10 22:38:28 +0530

    leetcode 13

--- Leetcode13.playground/Contents.swift
+++ Leetcode13.playground/Contents.swift
@@ -1,3 +1,21 @@
-import UIKit
+class Solution {
+    func maxOperations(_ nums: [Int], _ k: Int) -> Int {
+        var unpaired = [Int: Int]()
+        var result = 0
 
-var greeting = "Hello, playground"
+        for num in nums {
+            let pair = k - num
+            if let count = unpaired[pair], count > 0 {
+                result += 1
+                unpaired[pair] = count - 1
+            } else {
+                unpaired[num, default: 0] += 1
+            }
+
+
+        }
+
+    return result
+
+    }
+}
