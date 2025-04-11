b3c4373748527bcc626248158bd0b5773d15c10a
Rankit <rankit.agarwal97@gmail.com>
2025-04-11 11:33:25 +0530

    leetcode 16

--- Leetcode16.playground/Contents.swift
+++ Leetcode16.playground/Contents.swift
@@ -1,3 +1,37 @@
-import UIKit
+class Solution {
+    func longestOnes(_ nums: [Int], _ k: Int) -> Int {
+        var flipLeft = k
+        var maxLength = 0
+        var lp = 0
+        var rp = 0
 
-var greeting = "Hello, playground"
+        while rp < nums.count {
+            if nums[rp] == 1 {
+                rp += 1
+                continue
+            }
+
+            if nums[rp] == 0 && flipLeft != 0 {
+                rp += 1
+                flipLeft -= 1
+                continue
+            }
+
+            let newMax = (rp - lp)
+
+            maxLength = max(newMax, maxLength)
+
+            if nums[rp] == 0 && flipLeft == 0 {
+                if nums[lp] == 0 {
+                    lp += 1
+                    flipLeft += 1
+                } else {
+                    lp += 1
+                }
+            }
+
+        }
+            let newMax = rp - lp
+            return max(newMax, maxLength)
+    }
+}
