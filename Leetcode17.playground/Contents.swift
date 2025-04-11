f308a9677f111bd8f3cc31b6449421974d1d84e9
Rankit <rankit.agarwal97@gmail.com>
2025-04-11 11:33:37 +0530

    leetcode 17

--- Leetcode17.playground/Contents.swift
+++ Leetcode17.playground/Contents.swift
@@ -1,3 +1,27 @@
-import UIKit
+class Solution {
+    func longestSubarray(_ nums: [Int]) -> Int {
+        var left = 0
+        var right = 0
+        var maxLength = 0
+        var zeroCount = 0
 
-var greeting = "Hello, playground"
+
+        while right < nums.count {
+            if nums[right] == 0 {
+                zeroCount += 1
+            }
+
+              while zeroCount > 1 {
+                if nums[left] == 0 {
+                    zeroCount -= 1
+                }
+                left += 1
+            }
+                
+            maxLength = max(maxLength, right - left)
+            right += 1
+        }
+        return maxLength
+
+    }
+}
