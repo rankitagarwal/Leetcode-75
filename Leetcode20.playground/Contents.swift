8a384e29a9977ad04587ef074354a75c944ee9e7
Rankit <rankit.agarwal97@gmail.com>
2025-04-14 23:32:10 +0530

    leetcode 20

--- Leetcode20.playground/Contents.swift
+++ Leetcode20.playground/Contents.swift
@@ -1,3 +1,8 @@
-import UIKit
+class Solution {
+    func findDifference(_ nums1: [Int], _ nums2: [Int]) -> [[Int]] {
+        let nums1Set = Set(nums1)
+        let nums2Set = Set(nums2)
 
-var greeting = "Hello, playground"
+        return [Array(nums1Set.subtracting(nums2Set)), Array(nums2Set.subtracting(nums1Set))]
+    }
+}
